bd352c19cf0e649f52364e9b5e16a043ea9a6a15
Rittika Roy <rittikaroy085@gmail.com>
2026-09-16 23:34:37 +0530

    Update scripts

--- cpp/stimulator.cpp
+++ cpp/stimulator.cpp
@@ -1,1 +1,54 @@
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+int main () {
+double velocity;
+const double g = 9.81;
+double angle;
+const double PI = 3.141592653589793;
 
+cout <<"Rocket Trajectory Stimulator" << endl;
+cout <<"Enter Initial Velocity(m/s):" << endl;
+cin>>velocity;
+
+cout << "Enter launch angle (degrees): ";
+cin >> angle;
+
+cout << "Velocity = " << velocity << " m/s" << endl;
+cout << "Angle = " << angle << " degrees" << endl;
+
+double angleRadians = angle * PI / 180.0;
+double vx = velocity * cos(angleRadians);
+double vy = velocity * sin(angleRadians);
+cout << "Horizontal velocity = " << vx << " m/s" << endl;
+cout << "Vertical velocity = " << vy << " m/s" << endl;
+
+double maxHeight = (vy * vy) / (2 * g);
+cout << "Maximum height = " << maxHeight << " m" << endl;
+
+double flightTime = (2 * vy) / g;
+cout << "Flight time = " << flightTime << " seconds" << endl;
+
+double range = vx * flightTime;
+cout << "Horizontal range = " << range << " m" << endl;
+
+double time = 0;
+
+ofstream file("data/trajectory.csv");
+file << "Time,X,Y\n";
+while (time <= flightTime) {
+
+    double x = vx * time;
+    double y = vy * time - 0.5 * g * time * time;
+
+    cout << "Time: " << time
+         << " s | X: " << x
+         << " m | Y: " << y
+         << " m" << endl;
+    file << time << "," << x << "," << y << "\n";
+
+    time +=0.1;
+}
+file.close();
+return 0;
+}
--- python/plot.py
+++ python/plot.py
@@ -1,1 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv('cpp\\trajectory.csv')
 
+print(data)
+
+x = data["X"]
+y = data["Y"]
+
+print(x)
+print(y)
+
+max_height = y.max()
+max_height_index = y.idxmax()
+
+max_x = x[max_height_index]
+
+print("Maximum Height:", max_height, "m")
+print("Maximum Height occurs at X =", max_x, "m")
+
+plt.plot(x, y)
+
+plt.scatter(max_x, max_height)
+
+plt.annotate(
+    f"Max Height = {max_height:.2f} m",
+    (max_x, max_height)
+)
+
+range_distance = x.iloc[-1]
+
+print("Horizontal Range:", range_distance, "m")
+plt.xlabel("Horizontal Distance (m)")
+plt.ylabel("Height (m)")
+plt.title("Rocket Trajectory")
+
+plt.grid()
+plt.show()
